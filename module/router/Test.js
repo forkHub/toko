0f042db2df9014aa218c3a08d9a322f148821d46
fajar <rokhman.fajar@gmail.com>
2020-09-26 08:44:47 +0700

    a

--- module/router/Test.js
+++ module/router/Test.js
@@ -4,12 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const Log_1 = require("../Log");
+// import { logM } from "../Log";
 exports.router = express_1.default.Router();
 exports.router.get("/logm/:msg", (req, resp) => {
     try {
-        Log_1.logM.log(req.params.msg);
-        resp.send(Log_1.logM.logs);
+        // logM.log(req.params.msg);
+        // resp.status(200).send(logM.logs);
+        resp.status(200).send(req.params.msg);
     }
     catch (e) {
         console.log(e);
