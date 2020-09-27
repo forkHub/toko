e2621adcb155f784b0c84ad9bfdc54c20ea69f61
fajar <rokhman.fajar@gmail.com>
2020-09-27 07:54:46 +0700

    a

--- module/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    host: 'localhost',
-    user: 'root',
-    pass: '',
-    db: 'toko',
-    port: 3306
-};
