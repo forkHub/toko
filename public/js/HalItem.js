5b425e0e03119f3da736ec32ffa1ae98da08b790
fajar <rokhman.fajar@gmail.com>
2020-10-10 07:41:58 +0700

    update

---
+++ public/js/HalItem.js
@@ -0,0 +1,11 @@
+"use strict";
+window.onload = () => {
+    let img = document.body.querySelector("div.item-fokus img.besar");
+    img.onerror = () => {
+        console.log('error');
+        img.src = "/gambar/kosong.png";
+        img.onerror = null;
+        img.style.minHeight = '500px';
+    };
+    img.src = img.getAttribute('gbr');
+};
