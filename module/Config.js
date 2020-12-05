9d7b404b13edbc7fb32c0de2b02077608a13bcfe
fajar <rokhman.fajar@gmail.com>
2020-12-05 07:58:33 +0700

    config

---
+++ module/Config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    host: 'localhost',
+    user: 'root',
+    pass: '',
+    db: 'toko',
+    port: 3306,
+    sofwan: false,
+    jmlPerHal: 25,
+    namaToko: 'Auni Store',
+    moto: 'Belanja Mudah, Murah, Cepat dari Rumah'
+};
